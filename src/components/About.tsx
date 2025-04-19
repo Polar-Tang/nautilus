d0700cb572cd36ae8e1adc223f5ec9af469ee588
marco <virtualnautilus@gmail.com>
2025-04-19 02:08:19 -0300

    banner

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,56 @@
+import TurbulenceTitle from "./TurbulenceTitle"
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useRef } from "react"
+
+
 const About = () => {
+    const aboutContainerRef = useRef<HTMLDivElement>(null)
+
+    gsap.registerPlugin(useGSAP)
+
+        
+useGSAP(() => {
+    const ctx = gsap.context(() => {
+      const items = gsap.utils.toArray(".about-item")
+  
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: aboutContainerRef.current,
+          start: "top 80%",
+          end: "bottom 60%",
+          scrub: true,
+        },
+      })
+  
+      items.forEach((item, i) => {
+        item &&
+        tl.to(item, {
+          rotate: gsap.utils.random(-4, 4),
+          yPercent: gsap.utils.random(-5, 5),
+          duration: 1,
+          ease: "sine.inOut",
+        }, i * 0.1)
+      })
+    }, aboutContainerRef)
+  
+    return () => ctx.revert()
+  }, [])
+
+
     return (
-        <div className="container mx-auto px-4 py-32">
+        <div ref={aboutContainerRef} className="container mx-auto px-4 py-32">
             <ul>
-            <h3 className="text-4xl font-bold mb-6">Frontend Eninering</h3>
-            <p className="text-lg mb-8">React Developing</p>
-            <p className="text-lg mb-8">Animations</p>
+            <TurbulenceTitle title={"Frontend Eninering"}/>
+
+            <p className="about-item text-lg mb-8">React Developing</p>
+            <p className="about-item text-lg mb-8">Animations</p>
             </ul>
             <ul>
-            <h3 className="text-4xl font-bold mb-6">Backend Eninering</h3>
-            <p className="text-lg mb-8">NodeJS, Golang</p>
-            <p className="text-lg mb-8">Owasp top ten</p>
+            <TurbulenceTitle title={"Backend Eninering"}/>
+            <p className="about-item text-lg mb-8">NodeJS, Golang</p>
+            <p className="about-item text-lg mb-8">Owasp top ten</p>
             </ul>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
-import About from '@/components/About'
-import ArrowPointDown from '@/components/ArrowPointDown'
-import SvgComponentNavbarSlime from '@/components/SvgComponentNavbarSlime'
-import Wavy from '@/components/Wavy'
+import { About, Wavy, Crab } from '@/components/'
+import { motion } from 'motion/react'
+
+
 import "@/output.css"
 
 const Home = () => {
     return (
         <>
-            <div className="z-0 h-dvh w-full overflow-hidden">
-                <div className="absolute inset-0 bg-sand">
+            <div className="relative flex flex-col h-dvh w-full overflow-hidden">
+                {/* Background Layer */}
+                <div className="absolute h-dvh w-full  inset-0 bg-sand z-0">
                     <div
                         className="absolute inset-0 opacity-30"
                         style={{
@@ -16,73 +17,32 @@ const Home = () => {
                             backgroundSize: "20px 20px",
                         }}
                     />
-
-                    {/* Subtle gradient overlay */}
-                    <div className="absolute inset-0 bg-gradient-to-b from-sand via-clsand to-clsand " />
+                    <div className="absolute inset-0 bg-gradient-to-b from-sand via-clsand to-clsand" />
                 </div>
 
-                {/* <div className="absolute bottom-0 left-0 right-0">
-                    <svg className="w-full h-16 text-white" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path
-                            d="M0,0 C150,60 350,0 500,30 C650,60 700,0 900,40 C1050,70 1200,20 1200,20 L1200,0 L0,0 Z"
-                            className="fill-current"
-                        />
-                    </svg>
-                </div> */}
-            </div>
-            {/* <div className="fixed inset-0 z-0 h-dvh w-full overflow-hidden">
-                <div className="absolute inset-0 bg-sand-300">
-                    <div
-                        className="absolute inset-0 opacity-30"
-                        style={{
-                            backgroundImage: `radial-gradient(circle, rgba(0,0,0,0.1) 1px, transparent 1px)`,
-                            backgroundSize: "20px 20px",
-                        }}
-                    />
-
-                    <div className="absolute inset-0 bg-gradient-to-b from-sand-200 via-amber-300 to-amber-400 opacity-40" />
+                <div className="flex items-start p-4 z-10">
+                    <motion.img 
+                      whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                      whileTap={{ scale: 0.9 }}
+                    src="/starfish.png" alt="Starfish" className="h-12 md:h-18 -rotate-15 w-auto" />
                 </div>
 
-                <div className="absolute top-0 left-0 right-0">
-                    <svg className="w-full h-16 text-white" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path
-                            d="M0,0 C150,60 350,0 500,30 C650,60 700,0 900,40 C1050,70 1200,20 1200,20 L1200,0 L0,0 Z"
-                            className="fill-current"
-                        />
-                    </svg>
+                <div className="flex-grow flex justify-center items-center z-10">
+                    <img src="/nautilus.png" alt="Logo" className="h-auto w-80 md:w-100" />
+                </div>
+
+                <div className="flex justify-end items-end p-5 z-10">
+                    <div className="relative">
+                        <Crab className="h-30 m-0" />
+                    </div>
                 </div>
             </div>
-            <div className="fixed inset-0 z-0 h-dvh w-full overflow-hidden">
-                <div className="absolute inset-0 bg-amber-300">
-                    <div
-                        className="absolute inset-0 opacity-30"
-                        style={{
-                            backgroundImage: `radial-gradient(circle, rgba(0,0,0,0.1) 1px, transparent 1px)`,
-                            backgroundSize: "20px 20px",
-                        }}
-                    />
-                     <div className="absolute inset-0 bg-gradient-to-b from-amber-200 via-amber-300 to-amber-400 opacity-40" />
-                </div>
 
-                <div className="flex-grow flex items-center justify-center md:top-3/4 .-translate-y-50">
-                    <img src='/nautilus1.png' className="bg-negro w-200" />
-                </div>
-                
 
-                <div className="w-full flex items-end justify-between pb-8 px-6 z-1">
-                    <div className="flex-shrink-0">
-                        <svg className="w-16 h-16" viewBox="0 0 24 24">
-                        </svg>
-                    </div>
 
-                    <div className="flex flex-col items-center ">
-                        <h5 className="text-azul mb-2 text-3xl ">Dig deep into the ocean</h5>
-                        <ArrowPointDown />
-                    </div>
-
-                    <div className="flex-shrink-0 w-16"></div>
-                </div>
-            </div> */}
             <div className="relative h-[200vh] bg-negro z-0">
                 <Wavy />
             </div>
